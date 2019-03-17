a337dea4c67fdf485a8472a293db753d3e9fd98f
Mohammad Jahanzaib Alam <mohammad.alam-2@student.manchester.ac.uk>
2019-03-17 07:35:42 +0000

    Initial, section 1 complete, section 2 started

---
+++ 2_Terminal_Hacker/Assets/Hacker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("What are you here to hack into? \n\n");
+        Terminal.WriteLine("Press 1 for a resident house \n");
+        Terminal.WriteLine("Press 2 for a factory \n");
+        Terminal.WriteLine("Press 3 for a bank \n\n");
+        Terminal.WriteLine("Enter your selection: ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
