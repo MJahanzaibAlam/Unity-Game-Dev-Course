64bb363845c692e69a501f38f6a03797fa52e1e7
Mohammad Jahanzaib Alam <jahanzaib@outlook.com>
2019-03-27 22:54:33 +0000

    Finish main menu and refactor input logic

--- 2_Terminal_Hacker/Assets/Hacker.cs
+++ 2_Terminal_Hacker/Assets/Hacker.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    // Game state
+    int level;
+    enum Screen { MainMenu, Password, Win };
+    Screen currentScreen;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,12 +18,59 @@ public class Hacker : MonoBehaviour
 
     void ShowMainMenu()
     {
+        currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
         Terminal.WriteLine("What are you here to hack into? \n\n");
         Terminal.WriteLine("Press 1 for a resident house \n");
         Terminal.WriteLine("Press 2 for a factory \n");
         Terminal.WriteLine("Press 3 for a bank \n\n");
         Terminal.WriteLine("Enter your selection: ");
+    }
+
+    void OnUserInput(string input)
+    {
+        if (input == "menu")
+        {
+            ShowMainMenu();
+        }
+        else if (currentScreen == Screen.MainMenu)
+        {
+            RunMainMenu(input);
+        }
+    }
+
+    private void RunMainMenu(string input)
+    {
+        if (input == "117")
+        {
+            Terminal.WriteLine("Please select a level, Master Chief!");
+        }
+        else if (input == "1")
+        {
+            level = 1;
+            StartGame();
+        }
+        else if (input == "2")
+        {
+            level = 2;
+            StartGame();
+        }
+        else if (input == "3")
+        {
+            level = 3;
+            StartGame();
+        }
+        else
+        {
+            Terminal.WriteLine("Please choose a valid level");
+        }
+    }
+
+    void StartGame()
+    {
+        Terminal.WriteLine("You have chosen level " + level);
+        Terminal.WriteLine("Please enter your password: ");
+        currentScreen = Screen.Password;
     }
 
     // Update is called once per frame
