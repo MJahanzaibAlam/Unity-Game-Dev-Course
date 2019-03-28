b054da642e4f42c8b7527595512f1b65eec5671b
Mohammad Jahanzaib Alam <jahanzaib@outlook.com>
2019-03-28 01:24:58 +0000

    Complete Section 2 Terminal Hacker

--- 2_Terminal_Hacker/Assets/Hacker.cs
+++ 2_Terminal_Hacker/Assets/Hacker.cs
@@ -1,14 +1,18 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    // Game configuration data
+    const string menuHint = "You may type menu at any time.";
+    string[] level1Passwords = { "Pet", "Bed", "Room", "Cat", "Dog" };
+    string[] level2Passwords = { "Tanks", "Uniform", "Chairs", "Production", "Workers" };
+    string[] level3Passwords = { "Paperless", "Automation", "Dispenser", "Construction", "Supercomputer" };
+
     // Game state
     int level;
     enum Screen { MainMenu, Password, Win };
     Screen currentScreen;
+    string password;
 
     // Start is called before the first frame update
     void Start()
@@ -33,49 +37,134 @@ public class Hacker : MonoBehaviour
         {
             ShowMainMenu();
         }
+        else if (input == "quit" || input == "close" || input == "exit")
+        {
+            Terminal.WriteLine("If on the web, close the tab");
+            Application.Quit();
+        }
         else if (currentScreen == Screen.MainMenu)
         {
             RunMainMenu(input);
+        }
+        else if (currentScreen == Screen.Password)
+        {
+            CheckPassword(input);
         }
     }
 
     private void RunMainMenu(string input)
     {
-        if (input == "117")
+        bool isValidLevelNumber = (input == "1" || input == "2" || input == "3");
+        
+        if (isValidLevelNumber)
+        {
+            level = int.Parse(input);
+            AskForPassword();
+        }
+        else if (input == "117") // easter egg
         {
             Terminal.WriteLine("Please select a level, Master Chief!");
-        }
-        else if (input == "1")
-        {
-            level = 1;
-            StartGame();
-        }
-        else if (input == "2")
-        {
-            level = 2;
-            StartGame();
-        }
-        else if (input == "3")
-        {
-            level = 3;
-            StartGame();
         }
         else
         {
             Terminal.WriteLine("Please choose a valid level");
+            Terminal.WriteLine(menuHint);
         }
     }
 
-    void StartGame()
+    void AskForPassword()
     {
-        Terminal.WriteLine("You have chosen level " + level);
-        Terminal.WriteLine("Please enter your password: ");
         currentScreen = Screen.Password;
+        Terminal.ClearScreen();
+        SetRandomPassword();
+        Terminal.WriteLine("Enter your password, hint: " + password.Anagram());
+        Terminal.WriteLine(menuHint);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetRandomPassword()
     {
-        
+        switch (level)
+        {
+            case 1:
+                password = level1Passwords[Random.Range(0, level1Passwords.Length)];
+                break;
+            case 2:
+                password = level2Passwords[Random.Range(0, level2Passwords.Length)];
+                break;
+            case 3:
+                password = level3Passwords[Random.Range(0, level3Passwords.Length)];
+                break;
+            default:
+                Debug.LogError("Invalid level number");
+                break;
+        }
+    }
+
+    void CheckPassword(string input)
+    {
+        if (input == password)
+        {
+            DisplayWinScreen();
+        }
+        else
+        {
+            AskForPassword();
+        }
+    }
+
+    void DisplayWinScreen()
+    {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        ShowLevelReward();
+        Terminal.WriteLine(menuHint);
+    }
+
+    void ShowLevelReward()
+    {
+        switch (level)
+        {
+            case 1:
+                Terminal.WriteLine("Look at the cat!");
+                Terminal.WriteLine("Play again for a greater challenge!");
+                Terminal.WriteLine(@"
+       /\_/\
+  /\  / o o \
+ //\\ \~(*)~/
+ `  \/   ^ /
+    | \|| ||  
+    \ '|| ||  
+     \)()-())
+"               );
+                break;
+            case 2:
+                Terminal.WriteLine("Look at the factory!");
+                Terminal.WriteLine("Play again for a greater challenge!");
+                Terminal.WriteLine(@"
+   i______i
+   I______I
+   I      I
+   I______I
+  /      /I
+ (______( I
+ I I    I I
+ I      I
+");
+                break;
+            case 3:
+                Terminal.WriteLine("Look at the money!");
+                Terminal.WriteLine(@"
+ _ __ ___   ___  _ __   ___ _   _ 
+| '_ ` _ \ / _ \| '_ \ / _ \ | | |
+| | | | | | (_) | | | |  __/ |_| |
+|_| |_| |_|\___/|_| |_|\___|\__, |
+                             __/ |
+                            |___/ 
+");
+                break;
+            default:
+                Debug.LogError("Invalid level number");
+                break;
+        }
     }
 }
